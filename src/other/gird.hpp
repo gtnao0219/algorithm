a01e48e3f327428123152fd5fa98ee11cdc8f322
naotaka nakane <n.nakane0219@gmail.com>
2022-12-29 22:59:30 +0900

    Add grid.

---
+++ src/other/gird.hpp
@@ -0,0 +1,5 @@
+constexpr int dx[4] = {1, 0, -1, 0};
+constexpr int dy[4] = {0, 1, 0, -1};
+
+constexpr int dx8[8] = {1, 0, -1, 0, 1, -1, 1, -1};
+constexpr int dy8[8] = {0, 1, 0, -1, 1, -1, -1, 1};
